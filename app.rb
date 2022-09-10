d5d69ae0ba87f02db482fd8d268658656e999f33
Aleksey Chendemerov <merkyzeze@tutanota.com>
2022-09-10 16:00:42 +0300

    Create basic CLI

---
+++ app.rb
@@ -0,0 +1,14 @@
+require_relative 'calculator'
+
+if ARGV.length != 1
+  puts "Usage: #{$0} [expression]"
+else
+  begin
+    calculator = Calculator.new
+    puts "Result: #{calculator.run(ARGV[0])}"
+  rescue => e
+      puts e.message
+  end
+end
+
+
---
+++ calculator.rb
@@ -0,0 +1,28 @@
+require_relative 'lexer'
+require_relative 'parser'
+require_relative 'evaluator'
+require_relative 'token_type'
+
+class Calculator
+  TOKEN_TYPES = [
+    TokenType.new(:add,         /\+/,           precedence: 10),
+    TokenType.new(:sub,         /-/,            precedence: 10),
+    TokenType.new(:mult,        /\*/,           precedence: 20),
+    TokenType.new(:div,         /\//,           precedence: 20),
+    TokenType.new(:decimal,     /(\d*\.)?\d+/,  precedence: 0),
+    TokenType.new(:integer,     /\d+/,          precedence: 0),
+    TokenType.new(:whitespace,  /\s+/,          ignore: true),
+    TokenType.new(:lparen,      /\(/,           precedence: 1),
+    TokenType.new(:rparen,      /\)/,           precedence: 1)
+  ].freeze
+
+  def initialize(token_types = TOKEN_TYPES)
+    @lexer = Lexer.new(token_types)
+    @parser = Parser.new
+    @evaluator = Evaluator.new
+  end
+
+  def run(expression)
+    @evaluator.(@parser.(@lexer.(expression)))
+  end
+end
