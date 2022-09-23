0d8a4e664772a8a336e69da95bdab956183ec90c
Aleksey Chendemerov <merkyzeze@tutanota.com>
2022-09-23 12:03:52 +0300

    Enhance specs

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -4,36 +4,36 @@ require_relative '../lib/calculator'
 class TestLexer < Test::Unit::TestCase
   def test_addition
     calculator = Calculator.new
-    assert_equal(0, calculator.run('(-1) + 1'))
-    assert_equal(0, calculator.run('0 + 0'))
-    assert_equal(2, calculator.run('1 + 1'))
+    assert_equal(0.0, calculator.run('(-1) + 1'))
+    assert_equal(0.0, calculator.run('0 + 0'))
+    assert_equal(2.0, calculator.run('1 + 1'))
     assert_equal(3.4, calculator.run('2.3 + 1.1'))
     assert_equal(-10, calculator.run('(-11) + 1'))
   end
 
   def test_substraction
     calculator = Calculator.new
-    assert_equal(0, calculator.run('0 - 0'))
-    assert_equal(1, calculator.run('3 - 2'))
+    assert_equal(0.0, calculator.run('0 - 0'))
+    assert_equal(1.0, calculator.run('3 - 2'))
     assert_equal(10.5, calculator.run('10.7 - 0.2'))
-    assert_equal(-15, calculator.run('(-8) - 7'))
+    assert_equal(-15.0, calculator.run('(-8) - 7'))
   end
 
   def test_multiplication
     calculator = Calculator.new
-    assert_equal(0, calculator.run('0 * 0'))
-    assert_equal(0, calculator.run('0 * 123'))
-    assert_equal(25, calculator.run('5 * 5'))
+    assert_equal(0.0, calculator.run('0 * 0'))
+    assert_equal(0.0, calculator.run('0 * 123'))
+    assert_equal(25.0, calculator.run('5 * 5'))
     assert_equal(12.4, calculator.run('4 * 3.1'))
-    assert_equal(-18, calculator.run('(-3) * 6'))
+    assert_equal(-18.0, calculator.run('(-3) * 6'))
   end
 
   def test_division
     calculator = Calculator.new
-    assert_equal(0, calculator.run('0 / 1'))
-    assert_equal(20, calculator.run('100 / 5'))
+    assert_equal(0.0, calculator.run('0 / 1'))
+    assert_equal(20.0, calculator.run('100 / 5'))
     assert_equal(4.8, calculator.run('24 / 5'))
-    assert_equal(-17, calculator.run('(-85) / 5'))
+    assert_equal(-17.0, calculator.run('(-85) / 5'))
   end
 
   def test_divide_by_zero
@@ -45,14 +45,14 @@ class TestLexer < Test::Unit::TestCase
 
   def test_pharentesis
     calculator = Calculator.new
-    assert_equal(10, calculator.run('(1 + 4) * 2'))
-    assert_equal(7, calculator.run('((((((5 + 2))))))'))
-    assert_equal(-5, calculator.run('(-5) + 0'))
+    assert_equal(10.0, calculator.run('(1 + 4) * 2'))
+    assert_equal(7.0, calculator.run('((((((5 + 2))))))'))
+    assert_equal(-5.0, calculator.run('(-5) + 0'))
   end
 
   def test_log
     calculator = Calculator.new
-    assert_equal(2, calculator.run('log 100 10'))
+    assert_equal(2.0, calculator.run('log 100 10'))
     assert_equal(Float::INFINITY, calculator.run('log 4 1'))
     assert_equal(0.5, calculator.run('log 10 100'))
   end
